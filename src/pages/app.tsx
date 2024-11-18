d704d872a752103913f809dd177dd0bb1fde72d4
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2024-11-19 00:24:19 +0330

    update height for mobile devices

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatList from "../components/partitions/chat-list";
 import ChatMessage from "../components/partitions/chat-message";
 import { ChatT } from "../types/chat";
 import { AppContext } from "../components/contexts/app-context";
 
 export default function MainApp() {
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const [chatDetails, setChatDetails] = useState<ChatT | undefined>();
   return (
     <AppContext.Provider value={{ chatDetails, setChatDetails }}>
-      <div className="md:flex flex-row gap-1 hidden h-screen w-full">
+      <div
+        className="md:flex flex-row gap-1 hidden w-full"
+        style={{ height: `${height}px` }}
+      >
         <div className="w-1/4 h-full overflow-auto">
           <ChatList />
         </div>
@@ -17,7 +34,10 @@ export default function MainApp() {
       <div className="md:hidden flex flex-col gap-1">
         <ChatList />
         {chatDetails && (
-          <div className="fixed top-0 left-0 w-screen h-[92vh] overflow-auto">
+          <div
+            className="fixed top-0 left-0 w-screen overflow-auto"
+            style={{ height: `${height}px` }}
+          >
             <ChatMessage />
           </div>
         )}
