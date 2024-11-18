502aa2bd56cac36b5b7becf120acadeca66a04a1
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2024-11-18 18:56:43 +0330

    update router

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Auth from "./pages/auth.tsx";
-import NotFound from "./pages/not-found.tsx";
 import MainApp from "./pages/app.tsx";
 
 const appSections = createBrowserRouter([
   {
-    path: "",
+    path: "/",
     element: <MainApp />,
-    errorElement: <NotFound />,
+    // errorElement: <NotFound />,
   },
   {
     path: "auth",
